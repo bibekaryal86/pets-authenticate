df0e734bb482af71c9c53f6080d5e09b1dded465
Bibek Aryal <bibekaryal86@gmail.com>
2023-04-20 09:48:27 -0700

    Update JJWT

--- app/src/main/java/pets/authenticate/App.java
+++ app/src/main/java/pets/authenticate/App.java
@@ -3,13 +3,18 @@
  */
 package pets.authenticate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+    logger.info("Finished pets-authenticate initialization...");
   }
 }
